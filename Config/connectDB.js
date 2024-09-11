2ab5521b88dc429f8bcc61b7668f4d0744dbde2b
shamnakp <kps830190@gmail.com>
2024-09-11 12:29:44 +0530

    connectDB modified

--- Config/connectDB.js
+++ Config/connectDB.js
@@ -1,30 +1,28 @@
 const mongoose = require("mongoose");
 
-const connectDB = () => {
+//const connectDB = () => {
+//  try {
+//    const connect = mongoose.connect(process.env.MONGODB_URL);
+//    //console.log(process.env.MONGODB_URI);
+//    console.log("mongodb connected");
+//  } catch (error) {
+//    console.log("error", error);
+//  }
+//};
+
+const connectDB = async () => {
   try {
-    const connect = mongoose.connect(process.env.MONGODB_URL);
-    //console.log(process.env.MONGODB_URI);
-    console.log("mongodb connected");
+    const conn = await mongoose.connect("mongodb+srv://kpshamna2020:SNuAdK8M1Y7OIhEZ@clusterts.ie5w6.mongodb.net/cyclo?retryWrites=true&w=majority&appName=ClusterTS", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log("error", error);
+    console.error(`Error: ${error.message}`);
+    process.exit(1); // Exit process with failure
   }
 };
+
 
 module.exports = connectDB;
 
-/*const dbConnect = async () => {
-  try {
-      await mongoose.connect("mongodb+srv://kpshamna2020:SNuAdK8M1Y7OIhEZ@clusterts.ie5w6.mongodb.net/?retryWrites=true&w=majority&appName=ClusterTS", {
-          useNewUrlParser: true,
-          //useUnifiedTopology: true,
-          ssl: true,
-          tlsAllowInvalidCertificates: true,
-      });
-      
-      console.log('db connected');
-  } catch (error) {
-      console.log('mongo db connection error', error);
-  }
-};
-
-module.exports = { dbConnect };*/
