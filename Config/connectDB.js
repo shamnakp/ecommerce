a03af783c3b5fea58cc639d324517a46a2442e42
shamnakp <kps830190@gmail.com>
2024-09-11 11:50:06 +0530

    Config modified

--- Config/connectDB.js
+++ Config/connectDB.js
@@ -1,12 +1,30 @@
 const mongoose = require("mongoose");
 
-const conntectDB = () => {
+const connectDB = () => {
   try {
-    const conn = mongoose.connect(process.env.MONGODB_URI);
+    const connect = mongoose.connect(process.env.MONGODB_URL);
+    //console.log(process.env.MONGODB_URI);
     console.log("mongodb connected");
   } catch (error) {
     console.log("error", error);
   }
 };
 
-module.exports = conntectDB;
+module.exports = connectDB;
+
+/*const dbConnect = async () => {
+  try {
+      await mongoose.connect("mongodb+srv://kpshamna2020:SNuAdK8M1Y7OIhEZ@clusterts.ie5w6.mongodb.net/?retryWrites=true&w=majority&appName=ClusterTS", {
+          useNewUrlParser: true,
+          //useUnifiedTopology: true,
+          ssl: true,
+          tlsAllowInvalidCertificates: true,
+      });
+      
+      console.log('db connected');
+  } catch (error) {
+      console.log('mongo db connection error', error);
+  }
+};
+
+module.exports = { dbConnect };*/
